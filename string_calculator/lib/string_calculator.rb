53ae0e33a360ae09e535406845e399d05887afb4
Kumar Anand <anandk@nurseio.com>
2024-04-09 18:31:45 +0530

    Refactor code, add check and test case of num in negative and fix indentation of code.

--- string_calculator/lib/string_calculator.rb
+++ string_calculator/lib/string_calculator.rb
@@ -1,9 +1,20 @@
+# frozen_string_literal: true
+
 # string_calculator.rb
 
 class StringCalculator
   def self.add(numbers)
-    return 0 if numbers.empty?
-    
-    numbers.split(',').map(&:to_i).sum
+    raise ArgumentError, 'Input cannot be nil' if numbers.nil?
+
+    parsed_numbers = extract_numbers(numbers)
+    if parsed_numbers.any?(&:negative?)
+      raise ArgumentError, "Negative numbers not allowed: #{parsed_numbers.select(&:negative?).join(', ')}"
+    end
+
+    parsed_numbers.sum
+  end
+
+  def self.extract_numbers(numbers)
+    numbers.split(',').map(&:to_i)
   end
 end
